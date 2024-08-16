600438f2ef84162c45e99457c0d6bfa428d24c2f
Robert Vázquez <52977024+RobertCoderDev@users.noreply.github.com>
2024-08-16 14:07:12 -0600

    Delete src/environments/environment.ts

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    supabase: {
-      publicKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0b3lheGx3Ym5leHhwcHN2bXF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIzNzg5MzIsImV4cCI6MjAzNzk1NDkzMn0.SW01gzvFBjTPTvVnKWCf1JLlWgnK4wYvQFqq-IcKl2o',
-      url: 'https://btoyaxlwbnexxppsvmqt.supabase.co'
-    }
-  };
