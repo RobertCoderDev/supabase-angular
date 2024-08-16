c7153315451e93c03fda245b9134682b2057ed9d
RobertCoder <roberto.smusic.1@gmail.com>
2024-08-16 17:07:53 -0600

    cambios en router para manejo de magic link

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,5 +4,6 @@ import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  // providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation())]
 };
