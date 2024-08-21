7150f0d66da5f8ee3c4b63fb7385ee098d0c98fb
RobertCoder <roberto.smusic.1@gmail.com>
2024-08-21 14:01:03 -0600

    implementacion descripcion de imagen

--- src/app/pages/file-upload/file-upload.component.ts
+++ src/app/pages/file-upload/file-upload.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UploadFilesService } from '../../services/upload-files.service';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { Session } from '@supabase/supabase-js';
 import { Router } from '@angular/router';
 import { ChangeDetectorRef } from '@angular/core';
-
 
 @Component({
   selector: 'app-file-upload',
@@ -14,20 +13,29 @@ import { ChangeDetectorRef } from '@angular/core';
   imports: [CommonModule],
   standalone: true
 })
-export class FileUploadComponent {
+export class FileUploadComponent implements OnInit {
 
-  files: { name: string, url: string }[] = [];
+  files: { name: string, url: string,  description:string}[] = [];
   session: Session | null = null;
   selectedFile: File | null = null;
   isLoading: boolean = false;
+  userUuid: string = "";
+  private pollingInterval: any = null; // Para manejar el intervalo de reintentos
+  newFiles: number = 0;
+  firstLoad: boolean = true;
 
-  constructor(private uploadFilesService: UploadFilesService, private readonly authService: AuthService, private router: Router,  private cdr: ChangeDetectorRef) {}
+  constructor(
+    private uploadFilesService: UploadFilesService,
+    private readonly authService: AuthService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.authService.sessionChanges.subscribe(session => {
       this.session = session;
       if (session) {
-        this.loadFiles();
+        this.loadData();
       }
     });
   }
@@ -39,38 +47,62 @@ export class FileUploadComponent {
 
   async uploadFile(): Promise<void> {
     if (this.selectedFile) {
+      this.firstLoad = false;
       this.isLoading = true;
       this.cdr.detectChanges();
       try {
-        const response = await this.uploadFilesService.uploadFile(this.selectedFile);
-        console.log('File uploaded:', response);
-        await this.loadFiles();
+        await this.uploadFilesService.uploadFile(this.selectedFile);
         this.selectedFile = null;
-        this.cdr.detectChanges();
+        this.startPolling(); // Inicia el intervalo de reintentos
       } catch (error) {
         console.error('Error uploading file:', error);
+        this.isLoading = false;
       }
     } else {
       console.warn('No file selected.');
     }
   }
 
-  async loadFiles(): Promise<void> {
-    this.isLoading = false;
-    try {
-      const { data, error } = await this.uploadFilesService.getFiles();
-      if (error) {
-        console.error('Error loading files:', error);
-      } else {
-        this.files = data.map(file => {
-          const url = this.uploadFilesService.getPublicUrl(file.name);
-          return { name: file.name, url: url };
-        });
-        this.cdr.detectChanges();
-      }
-    } catch (error) {
-      console.error('Error loading files:', error);
+  private startPolling() {
+    this.pollingInterval = setInterval(async () => {
+      await this.loadData();
+    }, 5000); // Reintentar cada 5 segundos
+  }
+
+  private stopPolling() {
+    if (this.pollingInterval) {
+      clearInterval(this.pollingInterval);
+      this.pollingInterval = null;
     }
   }
 
+  async loadData(): Promise<void> {
+    this.isLoading = true;
+    try {
+      const data = await this.uploadFilesService.getDataByUserUuid();
+      this.files = data;
+      console.log("files: ", this.files);
+      this.cdr.detectChanges();
+
+      if (!this.firstLoad) {
+
+        if (this.files.length == this.newFiles) {
+          console.log("No new files yet.");
+        }else{
+          this.stopPolling(); // Detener el intervalo una vez que se hayan cargado los datos
+          console.log("ciclo de carga detenida");
+          this.isLoading = false;
+          this.newFiles = this.files.length + 1;
+        }
+
+      }else{
+        this.newFiles = this.files.length;
+        this.isLoading = false;
+      }
+
+    } catch (error) {
+      console.error('Error loading files:', error);
+      this.isLoading = false;
+    }
+  }
 }
--- src/app/services/upload-files.service.ts
+++ src/app/services/upload-files.service.ts
@@ -16,7 +16,7 @@ export class UploadFilesService {
     const cleanedFileName = this.cleanFileName(file.name);
     const filePath = `users/${userId}/${Date.now()}_${cleanedFileName}`;
     console.log("file path", filePath);
-    
+
     return this.supabase.storage.from('files').upload(filePath, file);
   }
 
@@ -26,16 +26,32 @@ export class UploadFilesService {
       .toLowerCase()
       .replace(/[^a-z0-9.]/g, '_');
   }
-  
+
   getFiles() {
-    const userId = this.authService.getUserId(); 
+    const userId = this.authService.getUserId();
     return this.supabase.storage.from('files').list(`users/${userId}`);
   }
 
   getPublicUrl(fileName: string) {
-    const userId = this.authService.getUserId(); 
+    const userId = this.authService.getUserId();
     const { data } = this.supabase.storage.from('files').getPublicUrl(`users/${userId}/${fileName}`);
     return data.publicUrl;
   }
+
+
+    async getDataByUserUuid(): Promise<any> {
+      const userId = this.authService.getUserId();
+      const { data, error } = await this.supabase
+        .from('files_db')
+        .select('*')
+        .eq('user_uuid', userId);
+
+      if (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+      }
+
+      return data;
+    }
 
 }
